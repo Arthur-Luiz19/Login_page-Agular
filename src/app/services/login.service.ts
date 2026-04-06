3ce1bb42cc9d74c8cfc4e07935e699084f2c3c4b
Arthur Luiz <arthur.luiz11@hotmail.com>
2026-04-06 15:56:16 -0300

    feat: create login page

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { loginResponse } from '../types/login-response.type';
+import { tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  login(name: string, password: string){
+    return this.httpClient.post<loginResponse>("/login", {name, password}).pipe(
+      tap((value) => {
+        sessionStorage.setItem("token", value.token);
+      })
+    )
+
+  }
+}
